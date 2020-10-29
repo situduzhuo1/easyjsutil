bf89238cb212116b2dce5632936525602c688f8c
situduzhuo1 <39145273+situduzhuo1@users.noreply.github.com>
2020-10-29 09:05:09 +0800

    wlz202010290905
    集合更新属性结构转换方法

--- src/items/arry.js
+++ src/items/arry.js
@@ -1,4 +1,13 @@
 /**
  * 集合类型方法集
  */
-export default {};
+/**
+ * 转树形结构
+ * @param {待转换集合} arr 
+ * @param {根节点值} rootval 
+ * @param {子节点属性名称} childname
+ */
+function GetTree(arr,rootval,childname){
+  console.log(arr,rootval,childname);
+}
+export default {GetTree};
